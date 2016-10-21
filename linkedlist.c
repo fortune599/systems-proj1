410a9cfda34eebf6302a1fa8ca4d6757e9d0dba9
Fortune Soleil <fsoleil@stuy.edu>
2016-10-20 21:27:36 -0400

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "linkedlist.h"
+
+void print_list(struct song_node* l){
+  printf("[");
+  while (l){
+    if (l -> next == 0) printf("%s",l -> artist);
+    else printf("%s, ", l -> artist);
+    l = l -> next;
+  }
+  printf("]\n");
+}
+
+struct song_node* insert_front(char n[256], char a[256], struct song_node* l){
+  struct song_node* new = (struct song_node*)malloc(sizeof(struct song_node));
+  strncpy(new -> name, n, 256);
+  strncpy(new -> artist, a, 256);
+  new -> next = l;
+  return new;
+}
+
+struct song_node* insert_order(char n[256], char a[256], struct song_node* l){ 
+  if (!l)
+    return insert_front(n, a, l);
+  if (strcmp(n, l -> name) < 0)
+    return insert_front(n, a, l);
+    
+  song_node* new = (song_node*)malloc(sizeof(song_node));
+  strncpy(new -> name, n, 256);
+  strncpy(new -> artist, a, 256);
+  song_node* c = l;
+  
+  while (c){
+    if (!(c -> next)){
+      c -> next = new;
+      new -> next = 0;
+      return c;
+    }  
+    if ((strcmp(n, c -> name) > 0) && (strcmp(n, (c -> next) -> name) < 0)){
+	new -> next = c -> next;
+	c -> next = new;
+    }
+    c = c -> next;
+  }  
+  return l;
+}
+
+song_node* find_song(char n[256], song_node *l){
+  song_node* i;
+  for (song_node* i = l; i; i = i -> next)
+    if (strcmp(n, i -> name) == 0)   
+	  return i;
+  return NULL;
+}
+
+song_node* find_artist(char a[256], song_node *l){
+  song_node* i;
+  for (song_node* i = l; i; i = i -> next)
+    if (strcmp(a, i -> artist) == 0)   
+	  return i;
+  return NULL;
+}
+
+//song_node *random_node(song_node *l){
+
+song_node* remove_node(char n[256], song_node *l){
+  struct song_node* c = l;
+  struct song_node* p;
+  while (c){
+    if (strncmp(c -> name, n, 256) == 0){
+      if (c == l) 
+	l = c -> next;
+      else {
+	p -> next = c -> next;
+      }
+      free(c);
+      return l;
+    }
+    else {
+      p = c;
+      c = c -> next;
+    }
+  }
+  return NULL;
+}
+
+void free_list(song_node *l){
+  song_node* next = 0;
+  song_node* i;
+  for (song_node* i = l; i; i = next){
+    next = i -> next;
+    free(i);
+  }
+}
