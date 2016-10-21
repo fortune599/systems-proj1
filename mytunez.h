de7a9c7bf8466990cabc49e6f7747d10000db7d2
Fortune Soleil <fsoleil@stuy.edu>
2016-10-20 21:29:46 -0400

    Create mytunez.h

---
+++ mytunez.h
@@ -0,0 +1,18 @@
+#include "linkedlist.h"
+
+void add_song(song_node* table[26], char n[256], char a[256]);
+
+song_node* search_song(song_node* table[26], char n[256]);
+
+song_node* search_by_artist(song_node *table[26], char a[256]);
+
+void print_artist(song_node* table[26], char a[256]);
+
+void shuffle(song_node *table[26]);
+
+void delete_song(song_node *table[26], char n[256], char a[256]);
+
+void print_library(song_node *table[26]);
+
+void free_library(song_node *table[26]);
+
